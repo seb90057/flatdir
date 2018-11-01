a575b273a088bcf96816f2f0e0da2aa4c7b487e1
seb90057 <seb.delarue@gmail.com>
2018-11-01 01:28:58 +0100

    add of sh launcher

--- clean_folder.py
+++ clean_folder.py
@@ -4,38 +4,48 @@ import tools
 from filekind.MsgFile import MsgFile
 import datetime
 from filekind.ZipFile import ZipFile
-
-folder_path = 'C:/Users/sebde/PycharmProjects/project_mgt/data'
-
+import sys
 
 
-path_to_treat = tools.path_to_treat(folder_path)
+if __name__ == '__main__':
 
-print(path_to_treat)
+    try:
+        folder_path = sys.argv[1]
+    except:
+        print('no path defined')
 
-while len(path_to_treat) > 0:
-    for p in path_to_treat:
-        ext = tools.get_extension(p).lower()
-        print(ext)
-        if ext in ['msg', 'zip']:
-            if ext == 'msg':
-                msg = MsgFile(p)
-                msg.save_attachments()
-                del msg
-                tools.rename(p, '{}/{}/{}'.format(os.path.dirname(p),
-                                               'processed',
-                                               tools.add_suffix(os.path.basename(p), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))))
-            if ext == 'zip':
-                zip = ZipFile(p, timestamp=datetime.datetime.strptime(tools.get_ts_suffix(p), "%Y%m%d%H%M%S"))
-                zip.extract_all()
-                del zip
-                tools.rename(p, '{}/{}/{}'.format(os.path.dirname(p),
-                                               'processed',
-                                               tools.add_suffix(os.path.basename(p), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))))
-        else:
-            if tools.get_ts_suffix(p) is None:
-                tools.rename(p, '{}/{}'.format(os.path.dirname(p),
-                                               tools.add_suffix(os.path.basename(p),
-                                                                datetime.datetime.fromtimestamp(os.path.getctime(p)).strftime('%Y%m%d%H%M%S'))))
+    # folder_path = 'C:/Users/sebde/PycharmProjects/project_mgt/data'
 
     path_to_treat = tools.path_to_treat(folder_path)
+
+    print(path_to_treat)
+
+    if not os.path.isdir('{}/{}'.format(os.path.dirname(folder_path), 'processed')):
+        os.mkdir('{}/{}'.format(os.path.dirname(folder_path), 'processed'))
+
+    while len(path_to_treat) > 0:
+        for p in path_to_treat:
+            ext = tools.get_extension(p).lower()
+            print(ext)
+            if ext in ['msg', 'zip']:
+                if ext == 'msg':
+                    msg = MsgFile(p)
+                    msg.save_attachments()
+                    del msg
+                    tools.rename(p, '{}/{}/{}'.format(os.path.dirname(p),
+                                                   'processed',
+                                                   tools.add_suffix(os.path.basename(p), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))))
+                if ext == 'zip':
+                    zip = ZipFile(p, timestamp=datetime.datetime.strptime(tools.get_ts_suffix(p), "%Y%m%d%H%M%S"))
+                    zip.extract_all()
+                    del zip
+                    tools.rename(p, '{}/{}/{}'.format(os.path.dirname(p),
+                                                   'processed',
+                                                   tools.add_suffix(os.path.basename(p), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))))
+            else:
+                if tools.get_ts_suffix(p) is None:
+                    tools.rename(p, '{}/{}'.format(os.path.dirname(p),
+                                                   tools.add_suffix(os.path.basename(p),
+                                                                    datetime.datetime.fromtimestamp(os.path.getctime(p)).strftime('%Y%m%d%H%M%S'))))
+
+        path_to_treat = tools.path_to_treat(folder_path)
